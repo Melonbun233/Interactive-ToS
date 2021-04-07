45cf9bb7afe6c8b90026a3f45a193c12121ec2b0
chenzh <1578067068@qq.com>
2021-04-07 15:47:27 +0800

    add content in detailed ToS

--- src/pages/DetailedToSPage.js
+++ src/pages/DetailedToSPage.js
@@ -1,6 +1,8 @@
 import { SignalCellularConnectedNoInternet2Bar } from '@material-ui/icons';
+import 'fontsource-roboto';
 import React from 'react';
 import pages from '../pages';
+import data from '../data';
 
 import useStyles from '../styles/style';
 import { Grid, Box, Typography, Button } from '@material-ui/core';
@@ -9,23 +11,37 @@ import { Grid, Box, Typography, Button } from '@material-ui/core';
 const DetailedToSPage = (props) =>{
 
   const classes = useStyles();
-
+  const sectionData = data.sections[props.value];
+  const {pages} = sectionData;
+  const length = pages.length;
+  console.log(pages);
+  console.log(length);
   const handleAgree = (value) =>{
     props.setHasAgreed(value);
     props.goto(pages['SectionSelectionPage']);
   }
 
   return(
-    <Box bgcolor='background.default' height='100vh' className={classes.content}>
+    <Box bgcolor='background.default'  className={classes.content}>
       <Box>
         <div className={classes.title}>
           <Typography variant='h4'>Section {props.value + 1}</Typography>
         </div>
       </Box>
-      <Box display={props.value == 0 ? 'block':'none'}>
-        <h4>1. .....</h4>
-        <h4>2. ......</h4>
-        <h4>3. ......</h4>
+      <Box >
+        {
+          pages.map((items,idx)=>(
+            <Box key={idx} margin={2}>
+              <Typography variant='body1'>{idx+1}:</Typography>
+              {
+                items.detailToS.map((item,key)=>(
+                  (item != null)? <Typography variant='body1' key={key}>{item}</ Typography>:<div></div>
+                )
+                )
+              }
+            </Box>
+          ))
+        }
       </Box>
       <Grid>
         <Button 
